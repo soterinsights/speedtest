8c62a15d0338d7e2ceed620a3d081d65d518cfb2
snoj <snoj@snoj.us>
2012-12-17 13:32:26 -0600

    Did some code clean up in speed.js so it's easier to follow. Once again, tests don't run as soon as the page loads. Added remote IP address "page" to speed.js. Added current Mbps calculation/output. Changed the test output a little so it flows better for multiple tests and readability. Modified the speed calculations...again. Added a maximum size that can be served up. (see opts.maxSize).
    Signed-off-by: Josh Erickson <snoj@snoj.us>

--- speed.js
+++ speed.js
@@ -4,7 +4,8 @@ var fs = require("fs");
 
 
 var opts = {
-	"url": ["/","/download","/upload","/jquery.js","/speed.html","/jquery.ajax-progress.js"]
+	"url": ["/","/download","/upload","/jquery.js","/speed.html","/jquery.ajax-progress.js","/ip"]
+	,maxSize: 50
 	,"port": 8080
 };
 
@@ -13,60 +14,70 @@ httpd = http.createServer(function(req, res) {
 		
 	});
 	//http://thecodinghumanist.com/blog/archives/2011/5/6/serving-static-files-from-node-js
-	switch(opts.url.indexOf(url.parse(req.url.replace("//","/")).pathname)) {
-		case 0:
-			res.writeHead(301, {'Location': "./speed.html"});
-			res.end();
-			break;
-		case 3:
-		case 4:
-		case 5:
-			try {
-			stats = fs.lstatSync("."+url.parse(req.url).pathname); // throws if path doesn't exist
-		  } catch (e) {
-		  console.log(e);
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.write('404 Not Found\n');
-			res.end("404: file not found or more likely, you're trying to go somewhere you can't.");
-			return;
-		  }
-			var s = fs.createReadStream("." + url.parse(req.url).pathname);
-			res.on("end", function() { s.destroy(); });
-			s.on('error', function (e) {
-				console.log(req.url);
-				console.log(e);
+	req.on('end', function() {
+		switch(opts.url.indexOf(url.parse(req.url.replace("//","/")).pathname)) {
+			case 0:
+				res.writeHead(301, {'Location': "./speed.html"});
+				res.end();
+				break;
+			case 1: //download
+				var max = url.parse(req.url,true).query.size;
+				//if(typeof(max) == 'undefined') { max = 1; }
+				if(typeof(max) == 'undefined' || max > opts.maxSize) {
+					res.writeHead(500);
+					res.end("It's too big or NaN!");
+					break;
+				}
+				res.writeHead(200, {'Content-length': (1024*1024*max)});
+				//res.write("download..prepare for X MB!\r\n");
+				var b = new Buffer(1024);
+				b.fill(0x0);
+				//console.log(max);
+				for(var i = 0; i < 1024*max; i++) {
+					res.write(b);
+				}
+				res.end();
+				break;
+			case 2:
+				res.write("upload..prepared for X MB!\r\n");
+				res.end();
+				break;
+			case 3:
+			case 4:
+			case 5:
+				try {
+				stats = fs.lstatSync("."+url.parse(req.url).pathname); // throws if path doesn't exist
+			  } catch (e) {
+			  console.log(e);
 				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.write('404 Not Found\n');
 				res.end("404: file not found or more likely, you're trying to go somewhere you can't.");
-			});
-			s.once('fd', function() {
-				res.statusCode = 400;
-				
-			});
-			s.pipe(res);s
-			break;
-		case 1:
-			var max = url.parse(req.url,true).query.size;
-			if(typeof(max) == 'undefined') { max = 1; }
-			res.writeHead(200, {'Content-length': (1024*1024*max)});
-			//res.write("download..prepare for X MB!\r\n");
-			var b = new Buffer(1024);
-			
-			//console.log(max);
-			for(var i = 0; i < 1024*max; i++) {
-				res.write(b);
-			}
-			res.end();
-			break;
-		case 2:
-			res.write("upload..prepare for X MB!\r\n");
-			res.end();
-			break;
-		default:
-			res.writeHead(400);
-			//res.statusCode = 400
-			res.write("fail");
-			res.end();
-	}
+				return;
+			  }
+				var s = fs.createReadStream("." + url.parse(req.url).pathname);
+				res.on("end", function() { s.destroy(); });
+				s.on('error', function (e) {
+					console.log(req.url);
+					console.log(e);
+					res.writeHead(404, {'Content-Type': 'text/plain'});
+					res.end("404: file not found or more likely, you're trying to go somewhere you can't.");
+				});
+				s.once('fd', function() {
+					res.statusCode = 400;
+					
+				});
+				s.pipe(res);s
+				break;
+			case 6:
+				res.end(req.connection.remoteAddress);
+				break;
+			default:
+				res.writeHead(400);
+				//res.statusCode = 400
+				res.write("fail");
+				res.end();
+		}
+	});
 	//
 });
 httpd.listen(opts.port);
